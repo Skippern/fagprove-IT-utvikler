f468cf767753a31c44557ecb088d553a27ffd39b
Aun Johnsen <skippern@gimnechiske.org>
2024-02-23 15:44:25 +0100

    persistent drive paa mysql funker

--- db/backend.sql
+++ db/backend.sql
@@ -11,19 +11,20 @@ CREATE TABLE IF NOT EXISTS `users` (
     `email` varchar(128) NOT NULL COMMENT 'Email for newsletter and password recovery',
     `phone` varchar(16) NULL COMMENT 'phone number, optional',
     `newsletter` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Accepted newsletters',
-    PRIMARY KEY (`ID`)
+    PRIMARY KEY (`ID`),
+    UNIQUE KEY `userName` (`user`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='List of registered users';
 
-CREATE TABLE IF NOT EXISTS `objects` (
-    `ID` bigint NOT NULL AUTO_INCREMENT,
-    `name` varchar(128) NOT NULL COMMENT 'Object Name',
-    `diameter` float NOT NULL DEFAULT '0.0' COMMENT 'Object average diameter in m',
-    `velocity` float NOT NULL DEFAULT '0.0' COMMENT 'Object velocity in km/s',
-    `closestApproachTime` TIMESTAMP NOT NULL COMMENT 'Time of Closest Approach',
-    `potentiallyHazardous` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Is Object potentially Hazardous',
-    `closestDistance` int NOT NULL COMMENT 'Closest distance in megameters',
-    PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='List of Objects, cache from NASA with our data structure';
+-- CREATE TABLE IF NOT EXISTS `objects` (
+--     `ID` bigint NOT NULL AUTO_INCREMENT,
+--     `name` varchar(128) NOT NULL COMMENT 'Object Name',
+--     `diameter` float NOT NULL DEFAULT '0.0' COMMENT 'Object average diameter in m',
+--     `velocity` float NOT NULL DEFAULT '0.0' COMMENT 'Object velocity in km/s',
+--     `closestApproachTime` TIMESTAMP NOT NULL COMMENT 'Time of Closest Approach',
+--     `potentiallyHazardous` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Is Object potentially Hazardous',
+--     `closestDistance` int NOT NULL COMMENT 'Closest distance in megameters',
+--     PRIMARY KEY (`ID`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='List of Objects, cache from NASA with our data structure';
 
 -- CREATE TABLE IF NOT EXISTS `session` (
 --     `uID` int,
