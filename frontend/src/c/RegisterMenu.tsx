686fd5c6a1c0ff4a94d2059db5053eecada8af9a
Aun Johnsen <skippern@gimnechiske.org>
2024-02-29 14:19:08 +0100

    sanity sjekk av passord og brukernavn i registrering

--- frontend/src/c/RegisterMenu.tsx
+++ frontend/src/c/RegisterMenu.tsx
@@ -3,16 +3,23 @@ import React, { useState, useEffect } from 'react';
 
 export default function RegisterMenu() {
     const [username, setUsername] = useState<string>()
-    const [password, setPassword] = useState<string|null>()
+    const [password, setPassword] = useState<string|undefined>()
     const [password1, setPassword1] = useState<string>('')
     const [password2, setPassword2] = useState<string>('')
     const [email, setEmail] = useState<string>()
-    const [newsletter, setNewsletter] = useState<boolean>()
-    const [acceptGDPR, setAcceptGDPR] = useState<boolean>()
+    const [newsletter, setNewsletter] = useState<boolean>(true)
+    const [acceptGDPR, setAcceptGDPR] = useState<boolean>(false)
     const [config, setConfig] = useState<any>()
     const [api, setApi] = useState<string>('http://localhost:5000/api/v1')
+    const [canSend, setCanSend] = useState<boolean>(false)
     const nav = useNavigate();
 
+    function bool2int(test: boolean): number {
+        if (test) {
+            return 1
+        }
+        return 0
+    } 
     const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(event.target.value)
     }
@@ -24,6 +31,12 @@ export default function RegisterMenu() {
     }
     const handleMailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(event.target.value)
+    }
+    const handleNewsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewsletter(!newsletter)
+    }
+    const handleGDPRChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAcceptGDPR(!acceptGDPR)
     }
 
     async function handleSetCookie() {
@@ -53,10 +66,10 @@ export default function RegisterMenu() {
     }
 
     useEffect(() => {
-        if (password1 === password2) {
+        if (password1 === password2 && password1 !== '') {
             setPassword(password1)
         } else {
-            setPassword(null)
+            setPassword(undefined)
         }
     }, [password1, password2])
     useEffect(() => {
@@ -81,6 +94,57 @@ export default function RegisterMenu() {
             }
         }
     }, [config])
+    useEffect(() => {
+        console.log('U: ', username, ' P: ', password, ' G: ', acceptGDPR, ' E: ', email)
+        if (!acceptGDPR) {
+            setCanSend(false)
+            return
+        }
+        if (!password) {
+            setCanSend(false)
+            return
+        }
+        if (!username) {
+            setCanSend(false)
+            return
+        }
+        if (!email) {
+            setCanSend(false)
+            return
+        }
+        if (username) {
+            if (username.length < 4) {
+                setCanSend(false)
+                return
+            }
+        }
+        if (email) {
+            // check email to contain correct format
+            console.log(email.indexOf('@'))
+            console.log(email.lastIndexOf('.'))
+            if (!email.includes('@')) {
+                setCanSend(false)
+                return
+            }
+            if (!email.includes('.')) {
+                setCanSend(false)
+                return
+            }
+            if (email.lastIndexOf('.') < email.indexOf('@')) {
+                setCanSend(false)
+                return
+            }
+        }
+        if (password) {
+            //check password for format
+            if (password.length < 6) {
+                setCanSend(false)
+                return
+            }
+        }
+        console.log('CanSend: true')
+        setCanSend(true)
+    }, [username,password,acceptGDPR,email])
     return (
         <div>
             <div><Link to='/login'><button>&lt;</button></Link><img src='img/logo.png' alt='Logo'/></div>
@@ -90,9 +154,9 @@ export default function RegisterMenu() {
                 <input name='email' placeholder='E-Post' value={email} onChange={handleMailChange}/>
                 <input type='password' name='password1' placeholder='Passord' value={password1} onChange={handlePass1Change}/>
                 <input type='password' name='password2' placeholder='Gjenta passord' value={password2} onChange={handlePass2Change}/>
-                <input type='checkbox' name='newsletter' />Kan vi sende stuff?<br/>
-                <input type='checkbox' name='GDPR' required/> Aksepterer  <Link to='/GDPR'>GDPR og betingelser</Link>
-                <button onClick={handleSetCookie} disabled={!password}>Registrer og Log Inn</button>
+                <input type='checkbox' name='newsletter'  checked={newsletter} onChange={handleNewsChange} />Kan vi sende stuff?<br/>
+                <input type='checkbox' name='GDPR' checked={acceptGDPR} onChange={handleGDPRChange}/> Aksepterer  <Link to='/GDPR'>GDPR og betingelser</Link>
+                <button onClick={handleSetCookie} disabled={!canSend}>Registrer og Log Inn</button>
             </div>
         </div>
     )
